6dcbc8fa9ee897ab2186ee2e70bc0d284c5aaba3
farzad <farzad.baghery@gmail.com>
2023-03-03 22:18:21 +0100

    Added container workflow

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const package = require('../package.json');
+
+const PRODUCION_DOMAIN = process.env.PRODUCION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${PRODUCION_DOMAIN}/marketing/remoteEntry.js`
+            },
+            shared: package.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
